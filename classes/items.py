6bf75c9ea0648a298718f329a5472e57067b76c8
esevlogiev <evgenistefchov@abv.bg>
2014-09-06 14:20:17 +0300

    Update items.py

--- classes/items.py
+++ classes/items.py
@@ -1,23 +1,24 @@
 import sqlite3
 
+
 class Item:
-	def __init__(self, name, _type):
-		self.name = name
-		self._type = _type
+    def __init__(self, name, _type):
+        self.name = name
+        self._type = _type
 
-	@property
-	def get_type(self):
-		return self._type
+    @property
+    def get_type(self):
+        return self._type
 
-	@property
-	def get_name(self):
-		return self.name
+    @property
+    def get_name(self):
+        return self.name
 
-	def set_name(self, value):
-		if value == "":
-			print("Invalid null or empty value")
-		else:
-			self.name = name
+    def set_name(self, value):
+        if value == "":
+            print("Invalid null or empty value")
+        else:
+            self.name = name
 
-	def __str__(self):
-		return self.name + ' from type ' + self._type
+    def __str__(self):
+        return self.name + ' from type ' + self._type
