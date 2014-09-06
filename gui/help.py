3b3360c0b93b61b43df84c6265426350f06e7ca5
esevlogiev <evgenistefchov@abv.bg>
2014-09-06 17:24:04 +0300

    Update help.py

--- gui/help.py
+++ gui/help.py
@@ -8,16 +8,19 @@ from PyQt5.QtGui import QIcon, QPixmap
 import gui.main
 import resource_file
 
+
 class Help(QWidget):
-    def __init__( self ):
+    def __init__(self):
         super(Help, self).__init__()
         self.setupUi(self)
 
     def setupUi(self, Help):
         self.option_picker = QListWidget()
         self.option_picker.setStyleSheet("background-color:transparent")
-        self.option_picker.addItem(QListWidgetItem("FrontDesk System Description"))
-        self.option_picker.addItem(QListWidgetItem("Requirements for Clients data"))
+        self.option_picker.addItem(
+            QListWidgetItem("FrontDesk System Description"))
+        self.option_picker.addItem(
+            QListWidgetItem("Requirements for Clients data"))
         self.option_picker.addItem(QListWidgetItem("Menus"))
         self.option_picker.setFrameStyle(QFrame.NoFrame)
         self.information_shower = QTextEdit()
@@ -30,7 +33,7 @@ class Help(QWidget):
         self.option_picker.setCurrentRow(0)
         self.option_picker.itemPressed.connect(self.choose_option)
         description = open("C:/Users/Asus/Desktop/project_final/\
-                           description_files/program_description.txt","r")
+                           description_files/program_description.txt", "r")
         self.information_shower.setText(description.read())
 
         self.setWindowIcon(QIcon(QPixmap(':/images/hotel_icon.jpg')))
@@ -39,14 +42,14 @@ class Help(QWidget):
     def choose_option(self):
         if self.option_picker.currentRow() == 0:
             description = open('C:/Users/Asus/Desktop/project_final/\
-                               description_files/program_description.txt',"r")
+                               description_files/program_description.txt', "r")
             self.information_shower.setText(description.read())
         elif self.option_picker.currentRow() == 1:
             requirements = open("C:/Users/Asus/Desktop/project_final/\
-                                description_files/requirements_for_client_data.txt", "r")
+                                description_files/\
+                                requirements_for_client_data.txt", "r")
             self.information_shower.setText(requirements.read())
         elif self.option_picker.currentRow() == 2:
             menus = open("C:/Users/Asus/Desktop/project_final/\
                          description_files/menus.txt", "r")
             self.information_shower.setText(menus.read())
-
