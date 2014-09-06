3c240f1d9aeea6815c3d1420fa0e883966fed61d
esevlogiev <evgenistefchov@abv.bg>
2014-09-06 16:07:16 +0300

    Update add_new_client.py

--- gui/add_new_client.py
+++ gui/add_new_client.py
@@ -2,13 +2,13 @@ import sys
 import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QMessageBox,\
-                            QDialog, QGridLayout, QLayout
+from PyQt5.QtWidgets import (QLabel, QLineEdit, QPushButton, QMessageBox,
+                             QDialog, QGridLayout, QLayout)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QPixmap
 
 from management.client_management import add_client
-from management.room_management import  get_all_free_rooms_number
+from management.room_management import get_all_free_rooms_number
 from data_correctness import Validations
 from classes.client import Client
 import gui.main
@@ -16,7 +16,7 @@ import datetime
 
 
 class AddNewClient_Form(QDialog):
-    def __init__( self ):
+    def __init__(self):
         super(AddNewClient_Form, self).__init__()
         self.setupUi(self)
 
@@ -27,8 +27,7 @@ class AddNewClient_Form(QDialog):
         min_room_number = min(get_all_free_rooms_number())
         max_room_number = max(get_all_free_rooms_number())
         room_number_label = "Enter room number({0}-{1}):".format(
-                                                          str(min_room_number),
-                                                          str(max_room_number))
+                            str(min_room_number), str(max_room_number))
         self.room_number_label = QLabel(room_number_label)
         self.room_number_line_edit = QLineEdit()
         self.first_name_label = QLabel("First Name:")
@@ -51,8 +50,9 @@ class AddNewClient_Form(QDialog):
         layout.addWidget(self.room_number_line_edit, 3, 1)
         layout.addWidget(self.stay_label, 4, 0)
         layout.addWidget(self.stay_line_edit, 4, 1)
-        layout.addWidget(self.add_new_client_button, 5, 0, 1, 2, Qt.AlignCenter)
-        
+        layout.addWidget(self.add_new_client_button, 5, 0, 1, 2,
+                         Qt.AlignCenter)
+
         self.setLayout(layout)
         self.add_new_client_button.clicked.connect(self.add_button_click)
         self.layout().setSizeConstraint(QLayout.SetFixedSize)
@@ -68,11 +68,11 @@ class AddNewClient_Form(QDialog):
     def is_information_invalid(self, room_number, stay, phone_number,
                                first_name, last_name):
         return (not Validations.is_positive_integer(room_number) or
-               not Validations.is_positive_integer(stay) or
-               not Validations.is_phone(phone_number) or
-               not Validations.is_name(first_name) or
-               not Validations.is_name(last_name) or
-               not int(room_number) in get_all_free_rooms_number())
+                not Validations.is_positive_integer(stay) or
+                not Validations.is_phone(phone_number) or
+                not Validations.is_name(first_name) or
+                not Validations.is_name(last_name) or
+                not int(room_number) in get_all_free_rooms_number())
 
     def error_message(self, room_number, stay, phone_number,
                       first_name, last_name):
@@ -84,8 +84,8 @@ class AddNewClient_Form(QDialog):
         for i in range(len(data)):
             if not properties[i](data[i]):
                 result.append(messages[i] + ',')
-        if (not Validations.is_positive_integer(room_number) or 
-            not int(room_number) in get_all_free_rooms_number()):
+        if (not Validations.is_positive_integer(room_number) or
+           not int(room_number) in get_all_free_rooms_number()):
             result.append('room number' + ',')
         return ' '.join(result)
 
@@ -98,14 +98,15 @@ class AddNewClient_Form(QDialog):
 
         if self.is_information_invalid(room_number, stay, phone_number,
                                        first_name, last_name):
-            error_message = self.error_message(room_number, stay, phone_number,
-                                               first_name, last_name) 
+            error_message = self.error_message(room_number, stay,
+                                               phone_number, first_name,
+                                               last_name)
             QMessageBox(QMessageBox.Critical, "Error",
-                        "Invalid "  + error_message[:len(error_message) - 1] +\
+                        "Invalid " + error_message[:len(error_message) - 1] +
                         ". Correct it!!!").exec_()
             return
 
-        new_client = Client(first_name.capitalize(), last_name.capitalize(),\
+        new_client = Client(first_name.capitalize(), last_name.capitalize(),
                             phone_number, int(room_number), int(stay),
                             self.date_to_string(datetime.date.today()))
 
@@ -117,7 +118,8 @@ class AddNewClient_Form(QDialog):
         self.clients.append(new_client)
         if add_client(new_client, int(room_number)):
             QMessageBox(QMessageBox.Information, "Add New client",
-            "Congratulations. You successful added this client!!!").exec_()
+                        "Congratulations. You successfully" +
+                        " added this client!!!").exec_()
         else:
             QMessageBox(QMessageBox.Information, "Add New client",
                         "This client already exists!!!").exec_()
