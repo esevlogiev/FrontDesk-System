a37e752c6766ef73ef8044fa334a29772574a441
esevlogiev <evgenistefchov@abv.bg>
2014-09-06 18:38:09 +0300

    Update room_management_tests.py

--- tests/room_management_tests.py
+++ tests/room_management_tests.py
@@ -16,9 +16,10 @@ TEST_DATABASE_FILENAME = 'tests.db'
 
 import unittest
 
+
 class RoomManagementTest(unittest.TestCase):
     def __init__(self, *args):
-        unittest.TestCase.__init__(self, *args) 
+        unittest.TestCase.__init__(self, *args)
 
     def setUp(self):
         create_tables(TEST_DATABASE_FILENAME)
@@ -94,10 +95,9 @@ class RoomManagementTest(unittest.TestCase):
         self.assertTrue(add_new_room(Room(105, 5, 5, 'False'),
                         TEST_DATABASE_FILENAME), 'room can not be add')
         self.assertFalse(add_new_room(third_room, TEST_DATABASE_FILENAME),
-                        'room can not be add')
+                         'room can not be add')
         self.assertFalse(add_new_room(Room(102, 10, 10, 'False'),
-                        TEST_DATABASE_FILENAME), 'room can not be add')
-
+                         TEST_DATABASE_FILENAME), 'room can not be add')
 
     def test_added_rooms(self):
         self.delete_room_table_rows()
@@ -114,7 +114,7 @@ class RoomManagementTest(unittest.TestCase):
         cursor.execute('''Select * from Rooms''')
         add_rooms = cursor.fetchall()
         self.assertCountEqual([convert_room(room[0]) for room in add_rooms],
-                               rooms)
+                              rooms)
         connection.close()
 
     def test_release_room(self):
@@ -143,7 +143,7 @@ class RoomManagementTest(unittest.TestCase):
         add_new_room(Room(120, 20, 5, 'False'), TEST_DATABASE_FILENAME)
         add_new_room(Room(121, 30, 6, 'False'), TEST_DATABASE_FILENAME)
         client = Client('Roger', 'Federer', '0898671234', 120, 5, '2014-08-20')
-        other_client = Client('Rafael', 'Nadal', '0898671234', 121, 10,\
+        other_client = Client('Rafael', 'Nadal', '0898671234', 121, 10,
                               '2014-08-16')
 
         connection = sqlite3.connect(TEST_DATABASE_FILENAME)
@@ -153,7 +153,8 @@ class RoomManagementTest(unittest.TestCase):
         release_all_rooms(TEST_DATABASE_FILENAME)
 
         cursor.execute('''Select * from Rooms''')
-        rooms_rent = [convert_room(room[0]).is_rent for room in cursor.fetchall()]
+        rooms_rent = [convert_room(room[0]).is_rent
+                      for room in cursor.fetchall()]
         self.assertTrue(all(room_rent == 'False' for room_rent in rooms_rent))
         self.assertFalse(any(room_rent == 'True' for room_rent in rooms_rent))
         connection.close()
@@ -164,9 +165,9 @@ class RoomManagementTest(unittest.TestCase):
         other_bath_item = BathItems('shampoo', "2014-9-10", 20, 121)
         connection = sqlite3.connect(TEST_DATABASE_FILENAME)
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",\
+        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",
                        (bath_item, ))
-        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",\
+        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",
                        (other_bath_item, ))
         connection.commit()
 
@@ -176,7 +177,8 @@ class RoomManagementTest(unittest.TestCase):
 
         other_room_bath_items = bath_items_in_room(121, TEST_DATABASE_FILENAME)
         self.assertCountEqual([other_bath_item], other_room_bath_items)
-        self.assertNotEqual([bath_item, other_bath_item], other_room_bath_items)
+        self.assertNotEqual([bath_item, other_bath_item],
+                            other_room_bath_items)
 
         connection.close()
 
@@ -197,7 +199,7 @@ class RoomManagementTest(unittest.TestCase):
 
         other_room_food = food_in_room(121, TEST_DATABASE_FILENAME)
         self.assertCountEqual([other_food], other_room_food)
-        self.assertNotEqual([food, other_food], other_room_food)  
+        self.assertNotEqual([food, other_food], other_room_food)
 
         connection.close()
 
@@ -207,9 +209,9 @@ class RoomManagementTest(unittest.TestCase):
         other_furniture = Furniture('chair', 'good', 121)
         connection = sqlite3.connect(TEST_DATABASE_FILENAME)
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO Furniture(furniture) values (?)",\
+        cursor.execute("INSERT INTO Furniture(furniture) values (?)",
                        (furniture, ))
-        cursor.execute("INSERT INTO Furniture(furniture) values (?)",\
+        cursor.execute("INSERT INTO Furniture(furniture) values (?)",
                        (other_furniture, ))
         connection.commit()
 
@@ -229,9 +231,9 @@ class RoomManagementTest(unittest.TestCase):
         other_furniture = SleepingFurniture('bed', 'good', 6, 121)
         connection = sqlite3.connect(TEST_DATABASE_FILENAME)
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO SleepingFurniture(sf) values (?)",\
+        cursor.execute("INSERT INTO SleepingFurniture(sf) values (?)",
                        (furniture, ))
-        cursor.execute("INSERT INTO SleepingFurniture(sf) values (?)",\
+        cursor.execute("INSERT INTO SleepingFurniture(sf) values (?)",
                        (other_furniture, ))
         connection.commit()
 
@@ -241,12 +243,11 @@ class RoomManagementTest(unittest.TestCase):
         self.assertNotEqual([furniture, other_furniture], room_furniture)
 
         other_room_furniture = sleeping_furnitures_in_room(121,
-                               TEST_DATABASE_FILENAME)
+                                                           TEST_DATABASE_FILENAME)
         self.assertCountEqual([other_furniture], other_room_furniture)
         self.assertNotEqual([furniture, other_furniture], other_room_furniture)
 
         connection.close()
-
 
     def test_change_food(self):
         self.delete_food_table_rows()
@@ -266,14 +267,14 @@ class RoomManagementTest(unittest.TestCase):
 
         cursor.execute('''Select * from Food''')
         foods = [convert_to_food(food[0]) for food in cursor.fetchall()]
-        self.assertFalse(all(food.is_expire() == False for food in foods))
-        self.assertTrue(any(food.is_expire() == True for food in foods))
+        self.assertFalse(all(food.is_expire() is False for food in foods))
+        self.assertTrue(any(food.is_expire() is True for food in foods))
 
         change_food(TEST_DATABASE_FILENAME)
         cursor.execute('''Select * from Food''')
         foods = [convert_to_food(food[0]) for food in cursor.fetchall()]
-        self.assertTrue(all(food.is_expire() == False for food in foods))
-        self.assertFalse(any(food.is_expire() == True for food in foods))
+        self.assertTrue(all(food.is_expire() is False for food in foods))
+        self.assertFalse(any(food.is_expire() is True for food in foods))
         connection.close()
 
     def test_change_bath_item(self):
@@ -285,27 +286,27 @@ class RoomManagementTest(unittest.TestCase):
 
         connection = sqlite3.connect(TEST_DATABASE_FILENAME)
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",\
-                      (bath_item, ))
-        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",\
+        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",
+                       (bath_item, ))
+        cursor.execute("INSERT INTO BathItems(bath_items) values (?)",
                        (other_bath_item, ))
         connection.commit()
 
         cursor.execute('''Select * from BathItems''')
         bath_items = [convert_bath_item(bath_item[0])
                       for bath_item in cursor.fetchall()]
-        self.assertFalse(all(bath_item.has_to_replace() == False\
+        self.assertFalse(all(bath_item.has_to_replace() is False
                          for bath_item in bath_items))
-        self.assertTrue(any(bath_item.has_to_replace() == True\
+        self.assertTrue(any(bath_item.has_to_replace() is True
                         for bath_item in bath_items))
 
         change_bath_item(TEST_DATABASE_FILENAME)
         cursor.execute('''Select * from BathItems''')
-        bath_items = [convert_bath_item(bath_item[0])\
+        bath_items = [convert_bath_item(bath_item[0])
                       for bath_item in cursor.fetchall()]
-        self.assertTrue(all(bath_item.has_to_replace() == False\
+        self.assertTrue(all(bath_item.has_to_replace() is False
                         for bath_item in bath_items))
-        self.assertFalse(any(bath_item.has_to_replace() == True\
+        self.assertFalse(any(bath_item.has_to_replace() is True
                          for bath_item in bath_items))
         connection.close()
 
@@ -322,8 +323,8 @@ class RoomManagementTest(unittest.TestCase):
         add_bath_item(bath_item, TEST_DATABASE_FILENAME)
         add_bath_item(other_bath_item, TEST_DATABASE_FILENAME)
         cursor.execute('''Select * from BathItems''')
-        self.assertCountEqual([other_bath_item, bath_item], 
-                              [convert_bath_item(item[0])\
+        self.assertCountEqual([other_bath_item, bath_item],
+                              [convert_bath_item(item[0])
                                for item in cursor.fetchall()])
 
     def test_add_food(self):
@@ -339,8 +340,8 @@ class RoomManagementTest(unittest.TestCase):
         add_food(food, TEST_DATABASE_FILENAME)
         add_food(other_food, TEST_DATABASE_FILENAME)
         cursor.execute('''Select * from Food''')
-        self.assertCountEqual([other_food, food], 
-                              [convert_to_food(item[0])\
+        self.assertCountEqual([other_food, food],
+                              [convert_to_food(item[0])
                                for item in cursor.fetchall()])
 
     def test_add_furniture(self):
@@ -356,8 +357,8 @@ class RoomManagementTest(unittest.TestCase):
         add_furniture(furniture, TEST_DATABASE_FILENAME)
         add_furniture(other_furniture, TEST_DATABASE_FILENAME)
         cursor.execute('''Select * from Furniture''')
-        self.assertCountEqual([other_furniture, furniture], 
-                              [convert_to_furniture(item[0])\
+        self.assertCountEqual([other_furniture, furniture],
+                              [convert_to_furniture(item[0])
                                for item in cursor.fetchall()])
 
     def test_add_sleeping_furniture(self):
@@ -373,8 +374,8 @@ class RoomManagementTest(unittest.TestCase):
         add_sleeping_furniture(furniture, TEST_DATABASE_FILENAME)
         add_sleeping_furniture(other_furniture, TEST_DATABASE_FILENAME)
         cursor.execute('''Select * from SleepingFurniture''')
-        self.assertCountEqual([other_furniture, furniture], 
-                              [convert_to_sleeping_furniture(item[0])\
+        self.assertCountEqual([other_furniture, furniture],
+                              [convert_to_sleeping_furniture(item[0])
                                for item in cursor.fetchall()])
 
     def test_get_all_free_rooms_number(self):
@@ -388,12 +389,12 @@ class RoomManagementTest(unittest.TestCase):
 
         free_rooms = get_all_free_rooms_number(TEST_DATABASE_FILENAME)
         self.assertCountEqual([third_room.get_number, first_room.get_number],
-                               free_rooms)
+                              free_rooms)
         self.assertNotEqual([third_room.get_number], free_rooms)
         self.assertNotEqual([third_room.get_number,
                              first_room.get_number,
                              second_room.get_number],
-                             free_rooms)
+                            free_rooms)
 
     def test_get_all_rooms_number(self):
         self.delete_room_table_rows()
@@ -406,12 +407,11 @@ class RoomManagementTest(unittest.TestCase):
 
         rooms = get_all_rooms_number(TEST_DATABASE_FILENAME)
         self.assertNotEqual([third_room.get_number, first_room.get_number],
-                               rooms)
+                            rooms)
         self.assertNotEqual([third_room.get_number], rooms)
         self.assertCountEqual([third_room.get_number,
-                             first_room.get_number,
-                             second_room.get_number
-                            ], rooms)
+                               first_room.get_number,
+                               second_room.get_number], rooms)
 
     def test_get_room(self):
         self.delete_room_table_rows()
